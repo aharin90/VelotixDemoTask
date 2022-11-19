de1171cc6e1b81b957cce2bba299693bcc1c867a
Mark <aharin@ukr.net>
2022-11-20 00:53:56 +0200

    Test updates

--- src/test/java/com/example/velotixdemo/controller/ControllersTest.java
+++ src/test/java/com/example/velotixdemo/controller/ControllersTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,8 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HealthTest {
-
+@ActiveProfiles("test")
+public class ControllersTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,6 +29,20 @@ public class HealthTest {
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/health")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Healthy")));
+    }
+
+    @Test
+    void shouldSendFileAndReceive200OK() throws Exception {
+        String fileName = "sampleFile.txt";
+        MockMultipartFile sampleFile = new MockMultipartFile(
+                "file",
+                fileName,
+                "text/plain",
+                "WARN : 2021-01-29 23:00:36.120 HikariPool-1 housekeeper: HikariPool$HouseKeeper: HikariPool-1 - Thread starvation or clock leap detected (housekeeper delta=47m52s667ms).".getBytes()
+        );
+        MockMultipartHttpServletRequestBuilder multipartRequest =
+                MockMvcRequestBuilders.multipart("/logs");
+        mockMvc.perform(multipartRequest.file(sampleFile)).andExpect(status().isOk());
     }
 }
 
